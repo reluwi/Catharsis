326f500dbcaba65bfd8c5f30ef72c71c8304420d
Renzo Osmillo <osmillo08@gmail.com>
2024-12-09 18:54:14 +0800

    test tokenizer (digits)
    finished the test tokenizer for digits.

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 typedef enum {
     SEMI,
@@ -25,8 +27,50 @@ typedef struct {
 
 typedef struct {
     TypeLiteral type;
-    int value;
+    char *value;
 } TokenLiteral;
+
+TokenLiteral *generate_number(char current, FILE *file){
+    TokenLiteral *token = malloc(sizeof(TokenLiteral));
+    token->type = INT;
+    char *value = malloc(sizeof(char) * 8);
+    int value_index = 0;
+    
+    while(isdigit(current) && current != EOF){
+        if(!isdigit(current)){
+            break;
+        }
+        value[value_index] = current;
+        value_index++;
+        current = fgetc(file);
+    }
+
+    value[value_index] = '\0';
+    token->value = value; 
+    return token;
+}
+
+TokenKeyword *generate_keyword(char current, FILE *file){
+    TokenKeyword *token = malloc(sizeof(TokenKeyword));
+    char *keyword = malloc(sizeof(char) * 4);
+    int keyword_index = 0;
+
+    while(isalpha(current) && current != EOF){
+        keyword[keyword_index] = current;
+        current = fgetc(file);
+    }
+
+    char keyword_str[keyword_index];
+    for(int i = 0; i < keyword_index; i++){
+        keyword_str[i] = keyword[i];
+    }
+
+    if(strcmp(keyword, "exit")){
+        token->type = EXIT;
+    }
+
+    return token;
+}
 
 void lexer(FILE *file){
     char current = fgetc(file);
@@ -40,6 +84,20 @@ void lexer(FILE *file){
         }
         else if(current == ')'){
             printf("FOUND CLOSE PAREN\n");
+        }
+        else if(isdigit(current)){
+            TokenLiteral *test_token = generate_number(current, file);
+            printf("TEST TOKEN VALUE: %s\n", test_token->value);\
+
+            free(test_token->value);
+            free(test_token);
+
+            //printf("FOUND DIGIT: %d\n", current - '0');
+        }
+        else if(isalpha(current)){
+            TokenKeyword *test_keyword = generate_keyword(current, file);
+            
+            //printf("FOUND CHARACTER: %C\n", current);
         }
         current = fgetc(file);
     }
